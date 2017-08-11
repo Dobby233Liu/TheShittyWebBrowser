3aad561f78bec02634d55b77e92b30249c9cbf25
Liu-Wen-Yuan <15816141883@163.com>
2017-08-11 06:07:38 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +20,16 @@ namespace TheShittyWebBrowser
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
 
+        }
+        
+        private void webBrowser1_NewWindow2(object sender, WebBrowserNewWindow2EventArgs e)
+        {
+        if (System.Globalization.CultureInfo.InstalledUICulture.NativeName = "中文(中华人民共和国)")
+        {
+        this.close();
+        fm=new webBrowserzh()
+        fm.Show();
+        }
         }
         private void webBrowserzh_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
